db582af182d2f4b18be91163ae326b6d94fc6163
Kevin <kayaswp@gmail.com>
2022-03-01 14:02:00 +0630

    component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hi {{name}}</h1>
+  <!-- <Modal></Modal> -->
+  <Modal/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "./components/Modal.vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      name:"Kevin"
+    }
+  },
+  components:{
+    // Modal:Modal
+    Modal
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  h1{
+    color : blue;
+  }
 </style>
